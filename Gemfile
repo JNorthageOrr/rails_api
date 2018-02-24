4a2990fcb29db5386a504f4a7046122daf0b2be4
Justin NORR <jnorthageorr@gmail.com>
2018-02-23 20:57:50 -0500

    upgraded to Rails 5

--- Gemfile
+++ Gemfile
@@ -2,8 +2,11 @@ source 'https://rubygems.org'
 
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
-gem 'rails', '5.1.5'
+gem 'rails', '5.1.4'
 #gem 'rails', '4.2.6'
+# Use railties gem, its a dependency
+#gem 'railties', '~> 5.0', '>= 5.0.0.1'
+gem 'railties'
 # Use postgresql as the database for Active Record
 gem 'pg', '~> 0.20.0'
 # Use SCSS for stylesheets
@@ -11,7 +14,8 @@ gem 'sass-rails', '~> 5.0'
 # Use Uglifier as compressor for JavaScript assets
 gem 'uglifier', '>= 1.3.0'
 # Use CoffeeScript for .coffee assets and views
-gem 'coffee-rails', '~> 4.1.0'
+#gem 'coffee-rails', '~> 4.1.0'
+gem 'coffee-rails', '~> 4.2'
 # See https://github.com/rails/execjs#readme for more supported runtimes
 # gem 'therubyracer', platforms: :ruby
 
@@ -42,6 +46,7 @@ gem 'knock'
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug'
+  gem 'listen'
 end
 
 group :development do
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,6 @@
 Rails.application.routes.draw do
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  resources :patterns
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
-
-  # You can have the root of your site routed with "root"
-  # root 'welcome#index'
-  
-  
   # Basic config prior to API conversion 
   
   #post 'user_token' => 'user_token#create'
@@ -17,9 +10,9 @@ Rails.application.routes.draw do
   #get '/help',    to: 'static_pages#help'
   #get '/about',   to: 'static_pages#about'
   #get '/contact', to: 'static_pages#contact'
-  #get '/signup',  to: 'users#new'
-  #get '/signup',  to: 'users#create'
-  #post '/signup', to: 'users#create'
+  get '/signup',  to: 'users#new'
+  get '/signup',  to: 'users#create'
+  post '/signup', to: 'users#create'
   
 
   # Config after API conversion
@@ -29,53 +22,4 @@ Rails.application.routes.draw do
     post 'user_token' => 'user_token#create'
   end
 
-  
-  # Example of regular route:
-  #   get 'products/:id' => 'catalog#view'
-
-  # Example of named route that can be invoked with purchase_url(id: product.id)
-  #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
-
-  # Example resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
-
-  # Example resource route with options:
-  #   resources :products do
-  #     member do
-  #       get 'short'
-  #       post 'toggle'
-  #     end
-  #
-  #     collection do
-  #       get 'sold'
-  #     end
-  #   end
-
-  # Example resource route with sub-resources:
-  #   resources :products do
-  #     resources :comments, :sales
-  #     resource :seller
-  #   end
-
-  # Example resource route with more complex sub-resources:
-  #   resources :products do
-  #     resources :comments
-  #     resources :sales do
-  #       get 'recent', on: :collection
-  #     end
-  #   end
-
-  # Example resource route with concerns:
-  #   concern :toggleable do
-  #     post 'toggle'
-  #   end
-  #   resources :posts, concerns: :toggleable
-  #   resources :photos, concerns: :toggleable
-
-  # Example resource route within a namespace:
-  #   namespace :admin do
-  #     # Directs /admin/products/* to Admin::ProductsController
-  #     # (app/controllers/admin/products_controller.rb)
-  #     resources :products
-  #   end
 end
