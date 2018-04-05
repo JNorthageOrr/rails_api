196b31aa372ff35bc6f3fe4728fd2da43edd9db1
Justin NORR <jnorthageorr@gmail.com>
2018-04-05 11:03:08 -0400

    adds CORS configuration to config/application.rb

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,12 @@ module RailsApi
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
 
+    #enable CORS for React front end app:
+    config.action_dispatch.default_headers = {
+    	'Access-Control-Allow-Origin' => 'http://localhost:5000',
+    	'Access-Control-Request-Method' => %w{GET POST OPTIONS}.join(",")
+    }
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
